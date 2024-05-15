2ce0ef971ae5caa82a8a0af2509ae977d2cbea4b
iahub4metra <buhai.artem7@gmail.com>
2024-05-15 15:20:52 +0100

    Update render-functions.js

---
+++ src/js/render-functions.js
@@ -0,0 +1,47 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+export const renderImages = (values, element) => {
+    const markup = values
+        .map(value => {
+        return `<li class="list-item">
+                    <a href="${value.largeImageURL}">
+                        <img
+                            src="${value.webformatURL}"
+                            alt="${value.tags}"
+                            class="image"
+                        />
+                    </a>
+                    <div class="lower-box">
+                        <div>
+                            <h3 class="card-title">Likes</h3>
+                            <p>${value.likes}</p>    
+                        </div>
+                        <div>
+                            <h3 class="card-title">Views</h3>
+                            <p>${value.views}</p>    
+                        </div>
+                        <div>
+                            <h3 class="card-title">Comments</h3>
+                            <p>${value.comments}</p>    
+                        </div>
+                        <div>
+                            <h3 class="card-title">Downloads</h3>
+                            <p>${value.downloads}</p>    
+                        </div>
+                    </div>
+                </li>`
+        })
+        .join("");
+    element.insertAdjacentHTML('beforeend', markup);
+    
+};
+export const iziToastShow = () => {
+    iziToast.error({message:"Sorry, there are no images matching your search query. Please try again!", position: "topRight"})
+}
+
+export const showLoader = (element) => {
+    element.style.display = "inline-block";
+}
+export const hideLoader = (element) => {
+    element.style.display = "none";
+}
