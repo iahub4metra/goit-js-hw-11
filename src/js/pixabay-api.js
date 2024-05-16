225b61667209bc13ef38ebe39d7aa77b816cfa84
iahub4metra <buhai.artem7@gmail.com>
2024-05-16 14:48:01 +0100

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,4 @@
-import { iziToastShow } from "./render-functions";
-
-export const getRequestApi = (userKeyWords, element, foo, showLoaderFoo, hideLoaderFoo, loader) => {
-    showLoaderFoo(loader);
+export const getRequestApi = (userKeyWords) => {
     return fetch(`https://pixabay.com/api/?key=43749964-78fe686efe4660c3d41d86a93&q=${userKeyWords}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then(response => {
             if (!response.ok) {
@@ -11,15 +8,8 @@ export const getRequestApi = (userKeyWords, element, foo, showLoaderFoo, hideLoa
         })
         .then(images => {
             const data = images.hits;
-            if (data.length === 0) {
-                iziToastShow();
-            } else {
-                foo(data, element)
-            }
+            return data
+           
         })
         .catch(error => console.log(error))
-        .finally(() => {
-            hideLoaderFoo(loader);
-        })
 }
-
